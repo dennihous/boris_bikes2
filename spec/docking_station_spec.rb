e0e3eaec9fe40339afbed0eeff81e3779e6a35bd
dennis <denhouston1@icloud.com>
2022-03-12 21:52:30 +0000

    added functionality to bike and docking station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,12 @@
-require 'docking_station'
+require_relative 'docking_station'
 
 describe DockingStation do
   it 'should respond to release_bike' do
     expect(subject).to respond_to :release_bike
+  end
+
+  it 'should release a working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
 end
